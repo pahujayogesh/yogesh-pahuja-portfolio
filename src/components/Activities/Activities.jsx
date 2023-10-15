f87efbd6e490638c12da214c3b8251dc9f6aaa72
Yogesh Pahuja <pahujayogesh.s@gmail.com>
2023-10-15 22:20:03 +0530

    changes made to file

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -7,8 +7,8 @@ function Activities() {
     <section id='activities' className='section activities'>
     <h2 className='section__title'>Activities</h2>
     <div className='crt_detail'>
-        {activities.map((data)=>
-            <h4>{data.title}      <a
+        {activities.map((data,i)=>
+            <h4 key={i}>{data.title}      <a
             href={data.link}
             aria-label='live preview'
             className='link link--icon'
--- src/components/Certification/Certification.jsx
+++ src/components/Certification/Certification.jsx
@@ -8,9 +8,9 @@ function Certification() {
     <section id='certification' className='section certification'>
     <h2 className='section__title'>Certification</h2>
     <div className='crt_detail'>
-        {certifactes.map((data)=>
+        {certifactes.map((data,i)=>
 
-            <a href={data.link}>
+            <a href={data.link} key={i}>
                 <h4>{data.title}</h4>
                 <img src={data.link} alt=""/>
             </a>
